628148099d5c8109e74440f37ab8174584157031
cschuhmacher1995 <cschuhmacher1995@gmail.com>
2016-02-07 14:00:15 +0100

    Saving important data from (Interflex)-Excel in a DTO-List

--- src/main/java/com/mayobirne/Controller.java
+++ src/main/java/com/mayobirne/Controller.java
@@ -1,15 +1,11 @@
 package com.mayobirne;
 
+import com.mayobirne.dto.InterflexDTO;
 import javafx.application.HostServices;
 import javafx.fxml.FXML;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -19,7 +15,10 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Christian on 07.02.2016.
@@ -30,6 +29,8 @@ public class Controller {
     private HostServices hostServices;
 
     private File outputFile;
+
+    private List<InterflexDTO> interflexList;
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -47,10 +48,12 @@ public class Controller {
     }
 
     private void test() throws IOException, InvalidFormatException {
+
+        interflexList = new ArrayList<InterflexDTO>();
+
         XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(outputFile));
         XSSFSheet sheet = wb.getSheetAt(0);
         XSSFRow row;
-        XSSFCell cell;
 
         int rows = sheet.getPhysicalNumberOfRows();
         int cols = 0;
@@ -67,13 +70,34 @@ public class Controller {
         for (int i = 0; i < rows; i++) {
             row = sheet.getRow(i);
             if (row != null) {
-                for (int c = 0; c < cols; c++) {
-                    cell = row.getCell(c);
-                    if (cell != null) {
-                        System.out.println(cell.getCachedFormulaResultType());
-                    }
+                if (row.getCell(2).getCellType() != Cell.CELL_TYPE_BLANK) {
+                    InterflexDTO dto = new InterflexDTO();
+                    dto.setDay_WD_DD(row.getCell(0).getStringCellValue());
+                    dto.setStartTime(convertToTimeString(row.getCell(2).getDateCellValue()));
+                    dto.setEndTime(convertToTimeString(row.getCell(3).getDateCellValue()));
+                    interflexList.add(dto);
                 }
             }
         }
+        System.out.println("ende");
+    }
+
+    private String convertToTimeString(Date date) {
+
+        if (date == null) {
+            return "";
+        }
+
+        String hour = String.valueOf(date.getHours());
+        if (hour.length() == 1)  {
+            hour = "0" + hour;
+        }
+
+        String minute = String.valueOf(date.getMinutes());
+        if (minute.length() == 1) {
+            minute = "0" + minute;
+        }
+
+        return hour + ":" + minute + ":00";
     }
 }
---
+++ src/main/java/com/mayobirne/dto/InterflexDTO.java
@@ -0,0 +1,35 @@
+package com.mayobirne.dto;
+
+/**
+ * Created by Christian on 07.02.2016.
+ */
+public class InterflexDTO {
+
+    String day_WD_DD;
+    String startTime;
+    String endTime;
+
+    public String getDay_WD_DD() {
+        return day_WD_DD;
+    }
+
+    public void setDay_WD_DD(String day_WD_DD) {
+        this.day_WD_DD = day_WD_DD;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
